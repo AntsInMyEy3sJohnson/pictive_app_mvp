449d327c02887cfb5c41b7e450a2f5bba992d009
Nico Krieg <n.krieg123@googlemail.com>
2021-07-29 17:53:52 +0200

    Remove scaffolded stuff.

---
+++ lib/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+
+  const Homepage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text("Stuff will go here"),
+      ),
+    );
+  }
+
+}
