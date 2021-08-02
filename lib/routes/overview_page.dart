62d47b5f576e8d90b58fc57793ea5a9780dd2b96
Nico Krieg <n.krieg123@googlemail.com>
2021-08-02 13:09:03 +0200

    WIP: Incorporate image picker

--- lib/routes/overview_page.dart
+++ lib/routes/overview_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pictive_app_mvp/state/user_bloc.dart';
+import 'package:pictive_app_mvp/state/user_state.dart';
 
-class OverviewPage extends StatelessWidget {
+class OverviewPage extends StatefulWidget {
 
   static const String ROUTE_ID = "/overview";
 
   const OverviewPage();
+
+  @override
+  _OverviewPageState createState() => _OverviewPageState();
+}
+
+class _OverviewPageState extends State<OverviewPage> {
+
+  final List<XFile> _images = [];
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +24,30 @@ class OverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Pictive"),
         actions: [
+          // Disable this button if no pictures are currently present
           IconButton(icon: Icon(Icons.filter_alt_outlined), onPressed: () => print("I was pressed"))
         ],
       ),
-      body: const Center(
-        child: Text("Stuff will go here"),
+      body: Center(
+        child: BlocBuilder<UserBloc, UserState>(
+          builder: (context, state) {
+            if(_images.isEmpty) {
+              return const Text("No images yet.");
+            }
+            return GridView.builder(
+                gridDelegate: gridDelegate,
+                itemBuilder: itemBuilder);
+          },
+        ),
+      ),
+      floatingActionButton: Column(
+
       ),
     );
+  }
+
+  Widget _generateImagePreview() {
+
   }
 
 }
