dba34fe91ca13666e37d32d6c64f09c6a9009e9a
Nico Krieg <n.krieg123@googlemail.com>
2021-09-29 20:25:28 +0200

    Represent 'RegisterUser' widget as a private class.

--- lib/routes/registration_page.dart
+++ lib/routes/registration_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:graphql/client.dart';
+import 'package:pictive_app_mvp/graphql/g_client_wrapper.dart';
 import 'package:pictive_app_mvp/input_validation/generic_input_validation.dart';
+import 'package:pictive_app_mvp/widgets/centered_circular_progress_indicator.dart';
 import 'package:pictive_app_mvp/widgets/login_register_body.dart';
-import 'package:pictive_app_mvp/widgets/mutations/register_user.dart';
 import 'package:pictive_app_mvp/widgets/relative_vertical_sized_box.dart';
+import 'package:pictive_app_mvp/widgets/sized_button_child.dart';
+
+import 'login_page.dart';
 
 class RegistrationPage extends StatefulWidget {
   static const String routeID = "/register";
@@ -76,7 +81,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
               _registrationTriggered
                   ? ElevatedButton(
                       onPressed: null,
-                      child: RegisterUser(
+                      child: _RegisterUser(
                         _emailController.text,
                         _passwordController.text,
                       ),
@@ -115,3 +120,96 @@ class _RegistrationPageState extends State<RegistrationPage> {
     return GenericInputValidation.stringValidation(passwordRepetition);
   }
 }
+
+class _RegisterUser extends StatefulWidget {
+  final String email;
+  final String password;
+
+  const _RegisterUser(this.email, this.password);
+
+  @override
+  _RegisterUserState createState() => _RegisterUserState();
+}
+
+class _RegisterUserState extends State<_RegisterUser> {
+  static const String _createUserMutation = r'''
+    mutation CreateUserWithDefaultCollection($mail: String!, $password: String!) {
+      createUserWithDefaultCollection(mail: $mail, password: $password) {
+        users {
+          id
+          mail
+          ownedCollections {
+            id
+          }
+          sharedCollections {
+            id
+          }
+          defaultCollection {
+            id
+          }
+          ownedImages {
+            id
+          }
+        }
+      }
+    }
+    ''';
+
+  late final Future<QueryResult> _createUserFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _createUserFuture = GClientWrapper.getInstance().performMutation(
+      _createUserMutation,
+      <String, dynamic>{'mail': widget.email, 'password': widget.password},
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QueryResult>(
+      future: _createUserFuture,
+      initialData: QueryResult.unexecuted,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SizedButtonChild(CenteredCircularProgressIndicator());
+        } else if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasData) {
+          Future.delayed(
+            const Duration(milliseconds: 200),
+                () => _onRegistrationComplete(snapshot.data!),
+          );
+          return const SizedButtonChild(
+            Icon(
+              Icons.check,
+              color: Colors.white,
+            ),
+          );
+        }
+        return const Text("Please try again later.");
+      },
+    );
+  }
+
+  Future<void> _onRegistrationComplete(QueryResult queryResult) async {
+    await showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Success!"),
+        content: const Text(
+          "Registration successful. You can now log in with your email address and password.",
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Got it!"),
+          ),
+        ],
+      ),
+    );
+    Navigator.pushReplacementNamed(context, LoginPage.routeID);
+  }
+}
+
